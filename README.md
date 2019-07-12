6ff913f72c786d2416c3d25e5a38b439bfa2cef1
syhan11 <sueyoung.6311@gmail.com>
2019-07-12 16:57:25 -0400

    exercise 201 commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Spring Boot Exercises 201-204 from Boot Camp"
---
+++ src/main/java/com/example/demo/Springboot201bcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot201bcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot201bcApplication.class, args);
+	}
+
+}
